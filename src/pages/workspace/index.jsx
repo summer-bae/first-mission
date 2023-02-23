34995f9313f7fb7f18bd56db805389616ca07d84
배인경 <ink000706@naver.com>
2023-02-23 14:32:34 +0000

    Create : First commit

---
+++ src/pages/workspace/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './containers/Header';
+
+const Workspace = () => (
+	<div>
+		<Header />
+		<div>Workspace</div>
+		<div>
+			<div>file</div>
+			<div>chat</div>
+		</div>
+	</div>
+);
+
+export default Workspace;
